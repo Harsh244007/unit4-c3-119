f0470e2ad46d0b46a26621b8d79582d860ddedf8
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-03-21 10:16:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express= require("express");
 const app = express();
-const path = require("path");
 const port = 5000;
 const mongoose = require('mongoose')
 
@@ -8,7 +7,7 @@ const connect = ()=>{
     return mongoose.connect("mongodb+srv://harsh:harsh001@cluster0.84eyj.mongodb.net/Users?retryWrites=true&w=majority");}
 
 //  schemas
-   
+
     const userSchema = new mongoose.Schema(
         {
             firstName:{type:String, required:true,minLength:3,maxLength:30},
@@ -29,7 +28,7 @@ const connect = ()=>{
 
     const bookSchema = new mongoose.Schema(
         {
-            likes:{type:Number, required:true, default:0},
+            likes:{type:Number, required:true, default:0,range:{min:{type:Number, min:0},max:{type:Number, max:1}}},
             coverImage :{type:String, required:true,range:{max:{type:String, max: 1}}},
             content:{type:String, required:true},
             userId:{type:mongoose.Schema.Types.ObjectId,ref:"user", required:true},
@@ -75,10 +74,48 @@ const connect = ()=>{
 
 // CRUD
 
-app.get("/user", (req, res) => {
-    res.send("h")
-})
+app.post("/user",async(req,res)=>{
+    try {
+        const user = await User.create(req.body);
+        return res.status(200).send({ user});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
 
-app.listen(port,(req, res) => {
+app.post("/post",async(req,res)=>{
+    try {
+        const book = await Book.create(req.body);
+        return res.status(200).send({ book});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+app.post("/comment",async(req,res)=>{
+    try {
+        const comment = await Comment.create(req.body);
+        return res.status(200).send({ comment });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+app.get("/user:id",async(req,res)=>{
+    try {
+        const user= await User.find(req.params.id).limit(10).lean().exec()
+        return res.status(200).send({user: user,login:"login:successful"});
+    } catch (error) {
+        return res.status(500).send("login unsuccessful");
+    }
+});
+
+app.listen(port, async() => {
+    try{
+        await connect();
+    }
+    catch(error) {
+        console.log(error.message);
+    }
     console.log("listening on port " + port);
 });
